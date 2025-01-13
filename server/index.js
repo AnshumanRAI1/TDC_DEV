e8a51eb53ad86ce6fc0c747e8cc0bdc19f2f82a7
AnshumanRAI1 <iamuchihamadara001@gmail.com>
2025-01-13 12:59:26 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1455,7 +1455,9 @@ app.get('/api/test-twilio', async (req, res) => {
     });
   }
 });
-
+app.get('/', (req, res) => {
+  res.status(200).send('API is running');
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
